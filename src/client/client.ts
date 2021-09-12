f7c60a4bd36034ed7fd8555d7d042344c4020b03
n1c00o <git.n1c00o@gmail.com>
2021-09-12 12:48:56 +0200

    Start client implementation

---
+++ src/client/client.ts
@@ -0,0 +1,50 @@
+import { Client } from "discord.js";
+import { Logger } from "winston";
+import { newLogger } from "../utils";
+
+export class Bot {
+  public client: Client;
+  public logger: Logger;
+
+  public constructor(public readonly token: string, debug: boolean) {
+    this.logger = newLogger("bot", debug);
+
+    this.client = new Client({
+      shards: 0,
+      shardCount: 1,
+      userAgentSuffix: ["bugcenter"],
+      intents: [
+        "GUILDS",
+        "GUILD_MEMBERS",
+        "GUILD_BANS",
+        "GUILD_INTEGRATIONS", // metrics
+        "GUILD_MESSAGES",
+        "GUILD_MESSAGE_REACTIONS",
+        "GUILD_WEBHOOKS", // metrics
+        "GUILD_EMOJIS_AND_STICKERS", // metrics
+        "GUILD_INVITES", // metrics
+        "GUILD_VOICE_STATES",
+        // "GUILD_PRESENCES"
+      ],
+      ws: {
+        large_threshold: 50,
+      },
+      http: {
+        version: 9,
+      },
+    });
+
+    // TODO - initialize events
+  }
+
+  /**
+   * connect the client
+   */
+  public async connect(): Promise<void> {
+    this.logger.debug("Connecting client to the WebSocket...");
+
+    await this.client.login(this.token).then(() => {
+      this.logger.info("WebSocket connection was established!");
+    });
+  }
+}
