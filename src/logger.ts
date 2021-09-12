67fdbd87e0670a3146b18c4228ea06e8766452f7
n1c00o <git.n1c00o@gmail.com>
2021-09-12 11:41:22 +0200

    Add logger

---
+++ src/logger.ts
@@ -0,0 +1,29 @@
+import { createLogger, format, Logger, transports } from "winston";
+const { combine, timestamp, colorize, metadata, simple, errors, align } =
+  format;
+
+/**
+ * Create a new Logger
+ * @param label - label of the component creating a logger
+ * @param shouldPrintDebug - whether or not we should log debug and trace messages
+ * @returns
+ */
+export function newLogger(label: string, shouldPrintDebug: boolean): Logger {
+  return createLogger({
+    defaultMeta: {
+      kind: label,
+    },
+    format: combine(
+      timestamp(),
+      colorize(),
+      metadata(),
+      simple(),
+      errors({
+        stack: true,
+      }),
+      align(),
+    ),
+    level: shouldPrintDebug ? "trace" : "info",
+    transports: [new transports.Console()],
+  });
+}
