11dd2d653322ae10fd8fc65da95a082b771f57d9
Pierre <pierrelixenbuhl@gmail.com>
2021-09-20 21:26:14 +0200

    Rename interactionCreate.ts to interaction_create.ts

--- src/events/interaction_create.ts
+++ src/events/interaction_create.ts
@@ -1,4 +1,4 @@
-import { Interaction } from 'discord.js';
+import { ButtonInteraction, CommandInteraction, Interaction } from 'discord.js';
 import { CustomClient } from '../client';
 import { verificationSystem } from '../functions/verification_system'; 
 
@@ -6,5 +6,20 @@ import { verificationSystem } from '../functions/verification_system';
 export async function handleInteractionCreate(bot: CustomClient, interaction: Interaction): Promise<void> {
 	bot.logger.debug('Received InteractionCreate event');
 
-	await verificationSystem(bot, interaction);
+	if (interaction instanceof ButtonInteraction) {
+		await verificationSystem(bot, interaction);
+	}
+
+	if (interaction instanceof CommandInteraction) {
+		if(!bot.store.getCommand(interaction.commandName)) return
+		
+		try {
+			await bot.store.getCommand(interaction.commandName)?.do(interaction)
+		} catch (error) {
+			bot.logger.error(error);
+			interaction.reply({ content: 'There was an error while executing this command', ephemeral: true });
+		}
+	}
+
+	
 }
