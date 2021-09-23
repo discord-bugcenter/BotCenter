acbeb6a24ac83b716dcfd2f1c62a7e76550d67c6
AiroPi <47398145+AiroPi@users.noreply.github.com>
2021-09-23 18:24:06 +0200

    Add a syncSlash parameter.
    When set to true, at startup the bot will push all the commands to discord.
    To avoid ratelimit, it is set to false by default.
    
    Add properties subCommands & extendedOptions & options to Command to get respectively a list of Command, a list of ExtendedApplicationCommandOption and a list of ApplicationCommandOption

--- src/models/command.ts
+++ src/models/command.ts
@@ -1,4 +1,4 @@
-import { ApplicationCommand, ApplicationCommandOption, Channel, CommandInteraction, Role, Snowflake, User } from 'discord.js';
+import { ApplicationCommand, ApplicationCommandNonOptionsData, ApplicationCommandOption, ApplicationCommandSubCommandData, Channel, CommandInteraction, Role, Snowflake, User } from 'discord.js';
 import { Logger } from 'winston';
 import { CustomClient } from '../client';
 import { BUG_CENTER_GUILD_ID } from '../utils';
@@ -21,7 +21,7 @@ interface CommandParameters {
 export abstract class Command {
 	readonly name: string;
 	readonly description: string;
-	readonly options: ExtendedApplicationCommandOption[] | Command[] = [];
+	private readonly _options: ExtendedApplicationCommandOption[] | Command[] = [];
 
 	readonly allowedRoles: Snowflake[] = [];
 	readonly deniedRoles: Snowflake[] = [];
@@ -35,11 +35,13 @@ export abstract class Command {
 		this.name = parameters.name;
 		this.description = parameters.description;
 
-		if (parameters.options) this.options = parameters.options;
+		if (parameters.options) this._options = parameters.options;
 		if (parameters.allowedRoles) this.allowedRoles = parameters.allowedRoles;
 		if (parameters.deniedRoles) this.deniedRoles = parameters.deniedRoles;
 		if (parameters.allowedUsers) this.allowedUsers = parameters.allowedUsers;
 		if (parameters.deniedUsers) this.deniedUsers = parameters.deniedUsers;
+
+
 
 		this.logger = bot.logger;
 		this.bot = bot;
@@ -54,7 +56,55 @@ export abstract class Command {
 	};
 
 	get applicationCommand(): ApplicationCommand | undefined {
-		return this.bot.client.guilds.cache.get(BUG_CENTER_GUILD_ID)?.commands.cache.filter(cmd => cmd.name == this.name).first();
+		return this.bot.client.guilds.cache.get(BUG_CENTER_GUILD_ID)?.commands.cache.filter(cmd => cmd.name === this.name).first();
 	};
+
+	get options(): ApplicationCommandOption[] {
+		function transformOptions(options: ExtendedApplicationCommandOption[] | Command[]): ApplicationCommandOption[] {
+			let transformedOptions: ApplicationCommandOption[] = []
+			
+			for (const option of options) {
+				if (option instanceof Command) {
+					if (option._options[0] instanceof Command) {
+						transformedOptions.push({
+							type: 'SUB_COMMAND_GROUP',
+							name: option.name,
+							description: option.description,
+							options: transformOptions(option._options) as ApplicationCommandSubCommandData[] & ApplicationCommandOption[]
+						})
+					} else {
+						transformedOptions.push({
+							type: 'SUB_COMMAND',
+							name: option.name,
+							description: option.description,
+							options: transformOptions(option._options) as ApplicationCommandNonOptionsData[] & ApplicationCommandOption[]
+						})
+					}
+
+				} else {
+					transformedOptions.push(option);
+				}
+			}
+			return transformedOptions;
+		}
+		return transformOptions(this._options);
+	}
+
+	get subCommands(): Command[] | undefined {
+		if (this._options[0] instanceof Command) {
+			return this._options as Command[];
+		} else {
+			return undefined;
+		}
+	}
+
+	get extendedOptions(): ExtendedApplicationCommandOption[] | undefined {
+		if (!(this._options[0] instanceof Command)) {
+			return this._options as ExtendedApplicationCommandOption[]
+		} else {
+			return undefined;
+		}
+		
+	}
 }
 
