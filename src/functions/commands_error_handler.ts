cf5f3d1018dc59e73155be02c40e5167dac07872
Pierre <pierrelixenbuhl@gmail.com>
2021-11-01 15:56:00 +0100

    Fix typing

--- src/functions/commands_error_handler.ts
+++ src/functions/commands_error_handler.ts
@@ -3,7 +3,7 @@ import { CustomClient } from "../client/index";
 import { __ } from "../utils";
 
 
-export async function applicationCommandsError(bot: CustomClient, interaction: CommandInteraction, error: Error): Promise<void> {
+export async function applicationCommandsError(bot: CustomClient, interaction: CommandInteraction, error: Error | unknown): Promise<void> {
     bot.logger.error(error);
     await interaction.reply({ content: __('There was an error while executing this command'), ephemeral: true });
 }
