7a7eb19d2421c354b4af451a0e32adec0df08b28
Pierre <pierrelixenbuhl@gmail.com>
2021-11-02 00:06:58 +0100

    Fix some style issues

--- src/functions/commands_error_handler.ts
+++ src/functions/commands_error_handler.ts
@@ -1,9 +1,9 @@
-import { CommandInteraction } from "discord.js";
-import { CustomClient } from "../client/index";
-import { __ } from "../utils";
+import { CommandInteraction } from 'discord.js';
+import { CustomClient } from '../client/index';
+import { __ } from '../utils';
 
 
 export async function applicationCommandsError(bot: CustomClient, interaction: CommandInteraction, error: Error | unknown): Promise<void> {
-    bot.logger.error(error);
-    await interaction.reply({ content: __('There was an error while executing this command'), ephemeral: true });
+	bot.logger.error(error);
+	await interaction.reply({ content: __('There was an error while executing this command'), ephemeral: true });
 }
